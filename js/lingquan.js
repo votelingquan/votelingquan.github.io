92b89a0f9e134727e75937a0957e05ecfa92ea7b
kohterai <koh.terai@nyu.edu>
2015-04-08 22:57:22 +0200

    Testimonial text and image swap on click.

--- js/lingquan.js
+++ js/lingquan.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-    $("#ling-testimonial").click(function(){
+    $(".testimonial-face-item").click(function(){
         console.log("testOutput")
-        $(this).hide();
+        console.log(this.dataset.nameeng);
+        nameEnglish = this.dataset.nameeng;
+        nameArabic = this.dataset.namearb;
+        personTitle = this.dataset.title;
+        descriptionEng = this.dataset.desceng;
+        descriptionArb = this.dataset.descarb;
+        faceImage = 'url("' + this.dataset.img + '")';
+        console.log(faceImage)
+        //Need to know whether to change description for ling or quans testimonial section
+        containerDiv = $(document.getElementById($(this).parent().data('parentgroup')))
+
+        containerDiv.find(".testimonial-face").css({"background": faceImage, "background-size": "cover"})
+        containerDiv.find(".nameEng").text(nameEnglish)
+        containerDiv.find(".nameArb").text(nameArabic)
+        containerDiv.find(".testimonial-title").text(personTitle)
+        containerDiv.find(".testimonialEng").text(descriptionEng)
+        containerDiv.find(".testimonialArb").text(descriptionArb)
+
+
     });
 });
